4718c8bfa06da6dab32abae29b657e2041aed10c
john Brunelle <anon4949@gmail.com>
2024-08-14 13:45:11 -0400

    Initial Upload

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AllianceReactNativeBridge",
+    platforms: [
+        .iOS(.v12),
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "AllianceReactNativeBridge",
+            targets: ["AllianceReactNativeBridgeWrapper","Hermes"]),
+    ],
+    targets: [
+        .target(
+            name: "AllianceReactNativeBridgeWrapper",
+            dependencies: [
+                .target(name: "AllianceReactNativeBridge")`
+            ],
+            path: "AllianceReactNativeBridgeWrapper"
+        ),
+        .binaryTarget(
+            name: "AllianceReactNativeBridge",
+            url: "https://github.com/AdeptMobile/AllianceReactNativeBridge/releases/download/Tag-0.0.0/AllianceReactNativeBridge.xcframework.zip",
+            checksum: "134b0e98cb999411fc2f9abd272d3586f4224f8d4cee7346b3fc873c99b47133"
+        ),
+         .binaryTarget(
+            name: "Hermes",
+            url: "https://github.com/AdeptMobile/AllianceReactNativeBridge/releases/download/Tag-0.0.0/hermes.xcframework.zip",
+            checksum: "b0e8d9c39b0c95aa26d0e74b474d1ed0bf5e4cf24f2eabbc2feec2bbab989f1f"
+        )
+    ]
+)
