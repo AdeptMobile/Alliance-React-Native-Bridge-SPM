c1dbdb1883cf0db491ccafa8c31f2769a72219cd
John Brunelle <anon4949@gmail.com>
2024-09-18 13:47:51 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -24,12 +24,12 @@ let package = Package(
         ),
         .binaryTarget(
             name: "AllianceReactNativeBridge",
-            url: "https://github.com/AdeptMobile/Alliance-React-Native-Bridge-SPM/releases/download/1.1.0/AllianceReactNativeBridge.xcframework.zip",
+            url: "https://github.com/AdeptMobile/Alliance-React-Native-Bridge-SPM/releases/download/1.0.1/AllianceReactNativeBridge.xcframework.zip",
             checksum: "c9b5094f95e39d448bd7e8ddf7e4962bc9d2818004d32aaf40b1a5dc1eb24daa"
         ),
          .binaryTarget(
             name: "Hermes",
-            url: "https://github.com/AdeptMobile/Alliance-React-Native-Bridge-SPM/releases/download/1.1.0/hermes.xcframework.zip",
+            url: "https://github.com/AdeptMobile/Alliance-React-Native-Bridge-SPM/releases/download/1.0.1/hermes.xcframework.zip",
             checksum: "cb99c771310d9cc03e7eeb1648b0282c3e993612905b5b32cf859a6ed7a68db8"
         )
     ]
