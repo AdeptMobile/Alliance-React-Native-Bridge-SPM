bc7ead17b3cc86a23357a687a0895ac26b8aa55e
Wayne Ohmer <wayne@ohmer.me>
2025-05-08 15:23:36 -0700

    new framework

--- Package.swift
+++ Package.swift
@@ -24,12 +24,12 @@ let package = Package(
         ),
         .binaryTarget(
             name: "AllianceReactNativeBridge",
-            url: "https://github.com/AdeptMobile/Alliance-React-Native-Bridge-SPM/releases/download/1.1.5/AllianceReactNativeBridge.xcframework.zip",
-            checksum: "94485ef2aed4fa0e14c12e10cc39a04ee3c362492bd1bdfea70035788ad0fbf7"
+            url: "https://github.com/AdeptMobile/Alliance-React-Native-Bridge-SPM/releases/download/1.1.6/AllianceReactNativeBridge.xcframework.zip",
+            checksum: "f21e9c4b57a883536cc87adb3c39a28e412d8492d64db59e268acf2eddd67a27"
         ),
          .binaryTarget(
             name: "Hermes",
-            url: "https://github.com/AdeptMobile/Alliance-React-Native-Bridge-SPM/releases/download/1.1.5/hermes.xcframework.zip",
+            url: "https://github.com/AdeptMobile/Alliance-React-Native-Bridge-SPM/releases/download/1.1.6/hermes.xcframework.zip",
             checksum: "cb99c771310d9cc03e7eeb1648b0282c3e993612905b5b32cf859a6ed7a68db8"
         )
     ]
