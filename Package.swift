19f9a53cdf5cd7a41664903cfa89d74fb00e3f34
Wayne Ohmer <wayne@ohmer.me>
2025-07-22 12:03:02 -0700

    update to 1.1.9 image cacheing

--- Package.swift
+++ Package.swift
@@ -24,12 +24,12 @@ let package = Package(
         ),
         .binaryTarget(
             name: "AllianceReactNativeBridge",
-            url: "https://github.com/AdeptMobile/Alliance-React-Native-Bridge-SPM/releases/download/1.1.8/AllianceReactNativeBridge.xcframework.zip",
-            checksum: "5f6c9c451a9fc64ce94d665837382b0231c2a928283b618bc575383cde603c72"
+            url: "https://github.com/AdeptMobile/Alliance-React-Native-Bridge-SPM/releases/download/1.1.9/AllianceReactNativeBridge.xcframework.zip",
+            checksum: "b0bd31867297e5acddd3c1cb630c1cc4f8b6f778ded30790f74bdc6a8a2b4b07"
         ),
          .binaryTarget(
             name: "Hermes",
-            url: "https://github.com/AdeptMobile/Alliance-React-Native-Bridge-SPM/releases/download/1.1.8/hermes.xcframework.zip",
+            url: "https://github.com/AdeptMobile/Alliance-React-Native-Bridge-SPM/releases/download/1.1.9/hermes.xcframework.zip",
             checksum: "8ba465fdb278d24906a8de462e8541c838c75354e71854e99e9135f98a12f328"
         )
     ]
