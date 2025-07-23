df015f7f2919c2999a07425cf359b94c5df3a193
Wayne Ohmer <wayne@ohmer.me>
2025-07-23 06:48:02 -0700

    fix checksums

--- Package.swift
+++ Package.swift
@@ -24,13 +24,13 @@ let package = Package(
         ),
         .binaryTarget(
             name: "AllianceReactNativeBridge",
-            url: "https://github.com/AdeptMobile/Alliance-React-Native-Bridge-SPM/releases/download/1.1.9/AllianceReactNativeBridge.xcframework.zip",
+            url: "https://github.com/AdeptMobile/Alliance-React-Native-Bridge-SPM/releases/download/1.1.10/AllianceReactNativeBridge.xcframework.zip",
             checksum: "b0bd31867297e5acddd3c1cb630c1cc4f8b6f778ded30790f74bdc6a8a2b4b07"
         ),
          .binaryTarget(
             name: "Hermes",
-            url: "https://github.com/AdeptMobile/Alliance-React-Native-Bridge-SPM/releases/download/1.1.9/hermes.xcframework.zip",
-            checksum: "8ba465fdb278d24906a8de462e8541c838c75354e71854e99e9135f98a12f328"
+            url: "https://github.com/AdeptMobile/Alliance-React-Native-Bridge-SPM/releases/download/1.1.10/hermes.xcframework.zip",
+            checksum: "194fa1d96ee24f8fe5a1e2e5a564d54110326f718d58829d9b97eab4fc358efc"
         )
     ]
 )
