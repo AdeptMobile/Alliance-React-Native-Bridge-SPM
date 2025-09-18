78ddc298d7ce52d3bcbf40c8df032a53f7392bb9
Wayne Ohmer <wayne@ohmer.me>
2025-09-18 10:02:24 -0700

    update package to 1.1.20

--- Package.swift
+++ Package.swift
@@ -24,18 +24,18 @@ let package = Package(
         ),
         .binaryTarget(
             name: "AllianceReactNativeBridge",
-            url: "https://github.com/AdeptMobile/Alliance-React-Native-Bridge-SPM/releases/download/1.1.17/AllianceReactNativeBridge.xcframework.zip",
-            checksum: "e4cfb7b11f2a7e9c82f4aa39c1f356583952c09ffbe58f7044e86eb56e23ef4d"
+            url: "https://github.com/AdeptMobile/Alliance-React-Native-Bridge-SPM/releases/download/1.1.20/AllianceReactNativeBridge.xcframework.zip",
+            checksum: "2a96ae0dd9c25bcd884dd20376e3fdd44697370f865b8964634dbe4a3b6f4d5e"
         ),
          .binaryTarget(
             name: "Hermes",
-            url: "https://github.com/AdeptMobile/Alliance-React-Native-Bridge-SPM/releases/download/1.1.17/hermes.xcframework.zip",
-            checksum: "b95292f9b5f801a976cfb30dc227b2694803239bacf88f695b80aaadf1f4bfb5"
+            url: "https://github.com/AdeptMobile/Alliance-React-Native-Bridge-SPM/releases/download/1.1.20/hermes.xcframework.zip",
+            checksum: "de951bd55e7b636b95b019acdf19e464e362a7e97d5a94c4d0777089ff59c5c2"
         ),
         .binaryTarget(
            name: "BlazeSDK",
-           url: "https://github.com/AdeptMobile/Alliance-React-Native-Bridge-SPM/releases/download/1.1.17/BlazeSDK.xcframework.zip",
-           checksum: "124258a9ab4252a9ec484920697eba4f8d7c7e4798b9794a65dbcfaa24450977"
+           url: "https://github.com/AdeptMobile/Alliance-React-Native-Bridge-SPM/releases/download/1.1.20/BlazeSDK.xcframework.zip",
+           checksum: "3c6ea9b6ceea8ab6dac7a55c05bb8bbddad2bbac3bbfae475f2d2b7fe806631a"
        )
     ]
 )
